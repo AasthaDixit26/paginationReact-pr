cdca357038a6b1ea3db9053b8b73e7e526a7abcc
Aastha Dixit <aaasthadixit@gmail.com>
2024-05-29 15:01:39 +0530

    This is the pagination project in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactPaginate from "react-paginate";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_page=1&_limit=12"
+      );
+      const data = await res.json();
+      setItems(data);
+    };
+    getPost();
+  }, []);
+
+  const fetchData = async (currentPage) => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=12`
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const handlePageOnClick = async (data) => {
+    let currentPage = data.selected + 1;
+    const postPages = await fetchData(currentPage);
+    setItems(postPages);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row m-2">
+        {items.map((item) => {
+          return (
+            <div
+              className="col-sm-6 col-md-4 my-2"
+              style={{ width: "18rem" }}
+              key={item.id}
+            >
+              <div
+                className="card shadow-sm w-100"
+                style={{ minHeight: "225px" }}
+              >
+                <div className="card-body">
+                  <h3 className="card-title text-center">{item.title}</h3>
+                  <h5 className="card-subtitle mb-2 text-muted text-center">
+                    {item.body}
+                  </h5>
+                  <p className="card-text">{item.it}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={8}
+        breakLabel={"......"}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageOnClick}
+        containerClassName="pagination justify-content-center"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        activeClassName="active"
+      />
     </div>
   );
 }
